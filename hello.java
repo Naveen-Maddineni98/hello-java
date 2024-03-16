cc32da8c97bcacc971f8f905ffbb0a5d8f85f15a
Naveen Maddineni <m.navein6@gmail.com>
2024-03-16 01:27:32 -0500

    committing java file

---
+++ hello.java
@@ -0,0 +1,35 @@
+public class hello {
+    public static void main(String args[])
+    {
+        int nums[][] = new int [3][4];
+
+        for(int i=0;i<3;i++)
+        {
+            for (int j=0;j<=3;j++)
+            {
+               
+          nums [i][j] = (int)(Math.random()* 1000);
+        }
+    }
+        
+
+        for(int i=0;i<3;i++)
+        {
+            for (int j=0;j<=3;j++)
+            {
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for (int n[]:nums)
+        {
+            for (int m: n)
+            {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
